745b74b2ad210be0939168fa9bdddb935625f3ee
SanG009 <subedisangam183@gmail.com>
2025-12-10 14:49:49 +0545

    refactor: update GetCategoriesBloc to improve event handling and state management

--- fintrack_frontend/lib/screens/add_expense/blocs/get_categories_bloc/get_categories_bloc.dart
+++ fintrack_frontend/lib/screens/add_expense/blocs/get_categories_bloc/get_categories_bloc.dart
@@ -1,14 +1,22 @@
 import 'package:bloc/bloc.dart';
-import 'get_categories_event.dart';
-import 'get_categories_state.dart';
+import 'package:equatable/equatable.dart';
 import 'package:expense_repository/expense_repository.dart';
 
+part 'get_categories_event.dart';
+part 'get_categories_state.dart';
+
 class GetCategoriesBloc extends Bloc<GetCategoriesEvent, GetCategoriesState> {
-  final ExpenseRepository repo;
-  GetCategoriesBloc(this.repo) : super(GetCategoriesInitial()) {
+  ExpenseRepository expenseRepository;
+
+  GetCategoriesBloc(this.expenseRepository) : super(GetCategoriesInitial()) {
     on<GetCategories>((event, emit) async {
-      // stub: emit loaded immediately
-      emit(GetCategoriesLoaded());
+      emit(GetCategoriesLoading());
+      try {
+        List<Category> categories = await expenseRepository.getCategory();
+        emit(GetCategoriesSuccess(categories));
+      } catch (e) {
+        emit(GetCategoriesFailure());
+      }
     });
   }
 }
